4924ea4ca04ed14c4780141e994dfd122a721908
christopheralan88 <christopheralan88@gmail.com>
2018-03-19 04:29:10 -0400

    Changed HTML generated by AJAX callback function.

--- src/main/resources/static/js/serviceCalls.js
+++ src/main/resources/static/js/serviceCalls.js
@@ -10,36 +10,44 @@ function getChildFormula(id) {
         var parentOuterWidth = parseInt($( formattedId ).css('padding-left'));
 
         for (var i=0; i<data.childFormulas.length; i++) {
-            html += '<div class="form-group">';
+            if (i === 0) {
+                html += '<div style="display: inline-flex;">'; //class="form-group"
+            }
+
             html += '<p id="' + data.childFormulas[i].id + '">' + data.childFormulas[i].name + '</p>';
-            //html += '<br/>';
 
             html += '<input class="form-control" type="text" ';
             html += 'id="' + data.childFormulas[i].id + '" ';
             html += 'name="' + data.name + ' - ' + data.childFormulas[i].name + '" ';
-            html += 'placeholder="' + data.childFormulas[i].name + ' "/>'
+            html += 'placeholder="' + data.childFormulas[i].name + ' ">'
 
             if (data.childFormulas[i].hasChildren) {
                 html += '<img src="/images/drill_arrow.svg" height="30px" onclick="getChildFormula(' + data.childFormulas[i].id + ')"/>'
             }
 
-            var newFormulaName = data.childFormulas[i].name.replace(/\s/g, '');
-            html += '<button type="button" th:onclick="' + newFormulaName + '()">Calculate</button>';
-            html += '<label class="col-form-label" for="' + data.childFormulas[i].name + '-result">Result:</label>';
-            html += '<input class="form-control" type="text" id="' + data.childFormulas[i].name + '-result"/>';
+            //var newFormulaName = data.childFormulas[i].name.replace(/\s/g, '');
+            //html += '<button type="button" th:onclick="' + newFormulaName + '()">Calculate</button>';
 
-            html += '</div>';
 
-            // Insert new HTML.
-            $( formattedId ).closest('div').after(html);
+            // get parent formula name and add Calculate button if child formula is the last child formula.
+            if (i === data.childFormulas.length - 1) {
+                var parentFormula = $( formattedId )[0].name;
+                html += '<button type="button" th:onclick="' + parentFormula + '()">Calculate</button>';
+                html += '<label class="col-form-label" for="' + data.childFormulas[i].name + '-result">Result:</label>';
+                html += '<input class="form-control" type="text" id="' + data.childFormulas[i].name + '-result">';
+                html += '</div>';
 
-            // Indent child formula by parentOuterWidth + 50px.
-            var childFormattedId = '#' + data.childFormulas[i].id;
-            var newChildPaddingLeft = parentOuterWidth + 50;
-            $( childFormattedId ).parent().css('padding-left', newChildPaddingLeft + 'px');
+                // Insert new HTML.
+                $( formattedId ).closest('div').after(html);
+
+                // Indent child formula by parentOuterWidth + 50px.
+//                var childFormattedId = '#' + data.childFormulas[i].id;
+//                var newChildPaddingLeft = parentOuterWidth + 50;
+//                $( childFormattedId ).parent().css('padding-left', newChildPaddingLeft + 'px');
+            }
 
             // Reset variable for next loop.
-            html = "";
+            //html = "";
         }
       },
       dataType: 'json'
